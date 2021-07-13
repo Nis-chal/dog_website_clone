9f596c0d0fa4e77049fb8d9d705a2772686df124
Nischal Bade <nischalchan@gmail.com>
2021-07-13 16:30:46 +0545

    about and what we do section was made

--- script.js
+++ script.js
@@ -7,23 +7,39 @@ $(document).ready(function () {
 
 $(window).scroll(function () {
     let scroll = $(window).scrollTop();
+ 
+    console.log(scroll)
 
-    if (scroll >= 70) {
+    if (scroll >= 70 ) {
         $('nav').addClass("new-nav");
+
+        $('nav ul li a').css({"color":"black"})
+
+        $('.nav-heading span').addClass("black")
+        $('.nav-heading span>i').addClass("redish")
+
+
+
+        // $('nav ul li a').css({"color": "black"})
+
+        // $('.nav-heading span').css({"color":"black"})
+        // $('.nav-heading span>i').css({"color":"#F85A40"})
+
+       
+       
+     
     }
 
     else {
         $('nav').removeClass("new-nav");
+       
+
+        $('.nav-heading span').removeClass("black")
+        // $('nav ul li a').css({"color": "#fff"})
+
+        // $('.nav-heading span').css({"color":"#fff"})
+        // $('.nav-heading span>i').css({"color":"#fff"})
 
     }
 })
 
-$('nav a').click(function (link) {
-    link.preventDefault();
-
-    let target = $(this).attr('href')
-
-    $('html,body').animate({
-        scrollTop: $(target).offset().top
-    }, "slow");
-})
